e810d028119c97300c145fc95379e04295f0dd1c
D-D-F <defreitas-david@outlook.fr>
2023-01-19 00:43:14 +0100

    modif test

--- src/components/EventCard/index.js
+++ src/components/EventCard/index.js
@@ -29,10 +29,12 @@ const EventCard = ({
 );
 
 EventCard.propTypes = {
-  imageSrc: PropTypes.string.isRequired,
+  // eslint-disable-next-line react/require-default-props
+  imageSrc: PropTypes.string,
   imageAlt: PropTypes.string,
   date: PropTypes.instanceOf(Date).isRequired,
-  title: PropTypes.string.isRequired,
+  // eslint-disable-next-line react/require-default-props
+  title: PropTypes.string,
   small: PropTypes.bool,
   label: PropTypes.string.isRequired,
 };
