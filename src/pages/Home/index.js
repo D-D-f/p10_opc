f88cc7e214c2606fc821866a9cd191c1b8796dbf
D-D-F <defreitas-david@outlook.fr>
2023-01-24 14:17:54 +0100

    modif card

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -124,7 +124,6 @@ const Page = () => {
                 imageSrc={last?.cover}
                 title={last?.title}
                 date={new Date(last?.date)}
-                small
                 label="boom"
             />
           </div>
